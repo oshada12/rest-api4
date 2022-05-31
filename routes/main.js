e33feeefc8efd004a4387a57a625b69234becea5
KirBotz <92134398+KirBotz@users.noreply.github.com>
2022-05-31 07:33:07 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -15,9 +15,4 @@ router.get('/api/game', (req, res) => {
     res.sendFile(__path + '/views/game.html')
 })
 
-router.use(function (req, res) {
-res.status(404)
-.sendFile(__path + '/views/404.html')
-});
-
 module.exports = router
